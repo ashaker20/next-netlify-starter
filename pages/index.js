fc2be56be8db7c5af579e39c78f054864d41f235
ashaker20 <63584087+ashaker20@users.noreply.github.com>
2022-10-07 23:25:19 -0400

    done

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { fetchEntries } from 'util/contentfulPosts'
+import Post from '@components/Post'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <div className = "posts">
+        {posts.map((p) => {
+          return <Post key = {p.date} date = {p.date} image = {p.image.fields} title = {p.title} />
+        })}
+      </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries()
+  const posts = await res.map((p) => {
+    return p.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  } 
 }
